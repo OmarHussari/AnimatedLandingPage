a79b36f12c7de8ab193f3d5ef8a94c14e05637d3
Omar Hussari <ohussari@gmail.com>
2023-03-01 00:44:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+AOS.init({
+    delay: 200,
+    duration: 1500,
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
